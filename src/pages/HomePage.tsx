61acab1adf7e15442531f7c1df8778f6b2422f49
채영준 <157681635+cyjoon68@users.noreply.github.com>
2025-10-21 22:18:50 +0900

    Add HomePage component and update export
    Created HomePage.tsx with basic layout and header. Updated index.ts to export HomePage from the new file, correcting the import path.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/layout';
+
+export default function HomePage() {
+    return (
+        <div className="flex flex-col min-h-screen transition-colors duration-300">
+            <Header userid={''} data={''} />
+            <main
+                className={`rounded-t-3xl flex flex-col gap-3 flex-grow w-full h-full "
+                }`}
+            ></main>
+        </div>
+    );
+}
